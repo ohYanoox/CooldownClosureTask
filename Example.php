8232d6952b5bd66fe66a6d4242bb04ba2ebd5708
ohYanoox <87778340+ohYanoox@users.noreply.github.com>
2022-09-25 22:09:20 +0200

    Create Example.php

---
+++ Example.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace yournamespace;
+
+use pocketmine\event\Listener;
+use pocketmine\plugin\PluginBase;
+use yanoox\CooldownClosureTask;
+
+final class Example extends PluginBase implements Listener
+{
+    protected function onEnable(): void
+    {
+
+        $this->getScheduler()->scheduleRepeatingTask(new CooldownClosureTask(function (int $cooldown, bool $isOver /**any variable name you want */) {
+            var_dump($cooldown);
+            if($isOver) var_dump("it's over !");
+            //the task will be cancelled as soon as the cooldown reaches: 0. (see CooldownClosureTask, onRun())
+        }, 5/**every 20 ticks, the timer will be decremented by 1*/), 20);
+    }
+}
